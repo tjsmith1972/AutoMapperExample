3166a0a7dbffe1a2653ff982e9ac3fec0597327e
TJ Smith <tjsmith1972@gmail.com>
2015-11-24 07:34:40 -0500

    Initial Commit

---
+++ AutoMapperExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+
+namespace AutoMapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                PersonId = 123,
+                FirstName = "TJ",
+                LastName = "Smith",
+                DateOfBirth = Convert.ToDateTime("12/8/1972"),
+                NickName = "Teej"
+            };
+            Mapper.CreateMap<Person, Employee>()
+                .ForMember(dest => dest.EmployeeId, opt => opt.MapFrom(src => src.PersonId))
+                .ForMember(dest => dest.Dob, opt => opt.MapFrom(src => src.DateOfBirth));
+            var employee = Mapper.Map<Employee>(person);
+
+            Console.WriteLine("employee.EmployeeId = " + employee.EmployeeId);
+            Console.WriteLine("employee.FirstName = " + employee.FirstName);
+            Console.WriteLine("employee.LastName = " + employee.LastName);
+            Console.WriteLine("employee.Dob = " + employee.Dob.ToShortDateString());
+            Console.WriteLine("employee.ManagerId = " + employee.ManagerId);
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NickName { get; set; }    
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
---
+++ AutoMapperExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoMapperExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VITA Program")]
+[assembly: AssemblyProduct("AutoMapperExample")]
+[assembly: AssemblyCopyright("Copyright © VITA Program 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ff21fce-3ec2-4208-aaa5-623a00328ff6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
